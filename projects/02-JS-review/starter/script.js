2e884f735a8621fa8633c6aab32ecdcf37fb2a15
personal <codewithlinds@gmail.com>
2023-09-14 10:50:53 -0500

    Completed Section 4

--- projects/02-JS-review/starter/script.js
+++ projects/02-JS-review/starter/script.js
@@ -1,48 +1,48 @@
 const data = [
   {
     id: 1,
-    title: "The Lord of the Rings",
-    publicationDate: "1954-07-29",
-    author: "J. R. R. Tolkien",
+    title: 'The Lord of the Rings',
+    publicationDate: '1954-07-29',
+    author: 'J. R. R. Tolkien',
     genres: [
-      "fantasy",
-      "high-fantasy",
-      "adventure",
-      "fiction",
-      "novels",
-      "literature",
+      'fantasy',
+      'high-fantasy',
+      'adventure',
+      'fiction',
+      'novels',
+      'literature'
     ],
     hasMovieAdaptation: true,
     pages: 1216,
     translations: {
-      spanish: "El señor de los anillos",
-      chinese: "魔戒",
-      french: "Le Seigneur des anneaux",
+      spanish: 'El señor de los anillos',
+      chinese: '魔戒',
+      french: 'Le Seigneur des anneaux'
     },
     reviews: {
       goodreads: {
         rating: 4.52,
         ratingsCount: 630994,
-        reviewsCount: 13417,
+        reviewsCount: 13417
       },
       librarything: {
         rating: 4.53,
         ratingsCount: 47166,
-        reviewsCount: 452,
-      },
-    },
+        reviewsCount: 452
+      }
+    }
   },
   {
     id: 2,
-    title: "The Cyberiad",
-    publicationDate: "1965-01-01",
-    author: "Stanislaw Lem",
+    title: 'The Cyberiad',
+    publicationDate: '1965-01-01',
+    author: 'Stanislaw Lem',
     genres: [
-      "science fiction",
-      "humor",
-      "speculative fiction",
-      "short stories",
-      "fantasy",
+      'science fiction',
+      'humor',
+      'speculative fiction',
+      'short stories',
+      'fantasy'
     ],
     hasMovieAdaptation: false,
     pages: 295,
@@ -51,88 +51,88 @@ const data = [
       goodreads: {
         rating: 4.16,
         ratingsCount: 11663,
-        reviewsCount: 812,
+        reviewsCount: 812
       },
       librarything: {
         rating: 4.13,
         ratingsCount: 2434,
-        reviewsCount: 0,
-      },
-    },
+        reviewsCount: 0
+      }
+    }
   },
   {
     id: 3,
-    title: "Dune",
-    publicationDate: "1965-01-01",
-    author: "Frank Herbert",
-    genres: ["science fiction", "novel", "adventure"],
-    hasMovieAdaptation: true,
+    title: 'Dune',
+    publicationDate: '1965-01-01',
+    author: 'Frank Herbert',
+    genres: ['science fiction', 'novel', 'adventure'],
+    hasMovieAdaptation: false,
     pages: 658,
     translations: {
-      spanish: "",
+      spanish: ''
     },
     reviews: {
       goodreads: {
         rating: 4.25,
         ratingsCount: 1142893,
-        reviewsCount: 49701,
-      },
-    },
+        reviewsCount: 49701
+      }
+    }
   },
   {
     id: 4,
     title: "Harry Potter and the Philosopher's Stone",
-    publicationDate: "1997-06-26",
-    author: "J. K. Rowling",
-    genres: ["fantasy", "adventure"],
+    publicationDate: '1997-06-26',
+    author: 'J. K. Rowling',
+    genres: ['fantasy', 'adventure'],
     hasMovieAdaptation: true,
     pages: 223,
     translations: {
-      spanish: "Harry Potter y la piedra filosofal",
-      korean: "해리 포터와 마법사의 돌",
-      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
-      portuguese: "Harry Potter e a Pedra Filosofal",
+      spanish: 'Harry Potter y la piedra filosofal',
+      korean: '해리 포터와 마법사의 돌',
+      bengali: 'হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন',
+      portuguese: 'Harry Potter e a Pedra Filosofal'
     },
     reviews: {
       goodreads: {
         rating: 4.47,
         ratingsCount: 8910059,
-        reviewsCount: 140625,
+        reviewsCount: 140625
       },
       librarything: {
         rating: 4.29,
         ratingsCount: 120941,
-        reviewsCount: 1960,
-      },
-    },
+        reviewsCount: 1960
+      }
+    }
   },
   {
     id: 5,
-    title: "A Game of Thrones",
-    publicationDate: "1996-08-01",
-    author: "George R. R. Martin",
-    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    title: 'A Game of Thrones',
+    publicationDate: '1996-08-01',
+    author: 'George R. R. Martin',
+    genres: ['fantasy', 'high-fantasy', 'novel', 'fantasy fiction'],
     hasMovieAdaptation: true,
     pages: 835,
     translations: {
-      korean: "왕좌의 게임",
-      polish: "Gra o tron",
-      portuguese: "A Guerra dos Tronos",
-      spanish: "Juego de tronos",
+      korean: '왕좌의 게임',
+      polish: 'Gra o tron',
+      portuguese: 'A Guerra dos Tronos',
+      spanish: 'Juego de tronos'
     },
     reviews: {
       goodreads: {
         rating: 4.44,
         ratingsCount: 2295233,
-        reviewsCount: 59058,
+        reviewsCount: 59058
       },
       librarything: {
         rating: 4.36,
         ratingsCount: 38358,
-        reviewsCount: 1095,
-      },
-    },
-  },
+        reviewsCount: 1095
+      }
+    }
+  }
 ];
 
 function getBooks() {
@@ -140,5 +140,163 @@ function getBooks() {
 }
 
 function getBook(id) {
-  return data.find((d) => d.id === id);
+  return data.find(d => d.id === id);
 }
+
+// Destructuring Objects and Arrays
+// const book = getBook(3);
+// book;
+// // const title = book.title;
+// // const author = book.author
+
+// // Destructuring Objects
+// const { title, author, pages, publicationDate, hasMovieAdaptation, genres } =
+//   book;
+// console.log(title, author, pages);
+
+// // Destructuring Arrays
+// // const primaryGenre = genres[0];
+// // const secondaryGenre = genres[1];
+
+// const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+// console.log(primaryGenre, secondaryGenre);
+
+// // Rest/Spread Operator
+// console.log(otherGenres);
+// // Spread
+// const newGenres = [...genres, 'epic fantasy'];
+// console.log(newGenres);
+
+// const updatedBook = {
+//   ...book,
+//   // Overwriting an existing property
+//   pages: 1210,
+//   // Adding a new property
+//   moviePublicationDate: '2001-12-19'
+// };
+// console.log(updatedBook);
+
+// const summary = `${title}, a ${pages}-page long book, written by ${author} and published in ${
+//   publicationDate.split('-')[0]
+// }. The book has ${hasMovieAdaptation ? '' : 'not'} been adapted as a movie`;
+// summary;
+
+// const pagesRange = pages > 1000 ? 'over 1,000' : 'less than 1,000';
+// pagesRange;
+// console.log(`The book as ${pagesRange} pages`);
+
+// // Arrow Functions
+// // function getYear(str) {
+// //   return str.split('-')[0];
+// // }
+
+// const getYear = str => str.split('-')[0];
+// console.log(getYear(publicationDate));
+
+// console.log(true && 'Some string');
+// console.log(false && 'Some string');
+// console.log(hasMovieAdaptation && 'This book has a movie');
+// console.log('jonas' && 'Some string');
+// console.log(0 && 'Some string');
+
+// console.log(true || 'Some string');
+// console.log(false || 'some string');
+
+// console.log(book.translations.spanish);
+// const spanishTranslation = book.translations.spanish || 'NOT TRANSLATED';
+// spanishTranslation;
+
+// // console.log(book.reviews.librarything.reviewsCount);
+// // const countWrong = book.reviews.librarything.reviewsCount || 'no data';
+// // countWrong;
+// // const count = book.reviews.librarything.reviewsCount ?? 'no data';
+// // count;
+
+// function getTotalReviewCount(book) {
+//   const goodRead = book.reviews?.goodreads?.reviewsCount;
+//   const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+//   return goodRead + librarything;
+// }
+// console.log(getTotalReviewCount(book));
+
+// The Array map Method
+// const books = getBooks(1);
+
+// // map
+// const x = [1, 2, 3, 4, 5].map(el => el * 2);
+// console.log(x);
+
+// const titles = books.map(book => book.title);
+// console.log(titles);
+
+// const essentialData = books.map(book => ({
+//   title: book.title,
+//   author: book.author
+// }));
+// essentialData;
+
+// // filter
+// const longBooksWithMovie = books
+//   .filter(book => book.pages > 500)
+//   .filter(book => book.hasMovieAdaptation);
+// longBooksWithMovie;
+
+// const adventureBooks = books
+//   .filter(books => books.genres.includes('adventure'))
+//   .map(book => book.title);
+// adventureBooks;
+
+// // reduce
+// const pagesAllBooks = books.reduce((sum, book) => sum + book.pages, 0);
+// pagesAllBooks;
+
+// // sort
+// const arr = [3, 7, 1, 9, 6];
+// const sorted = arr.slice().sort((a, b) => a - b); // Ascending
+// sorted;
+// arr;
+
+// const sortedByPages = books
+//   .slice()
+//   .sort((a, b) => b.pages - a.pages)
+//   .map(book => book.title); // descending
+// sortedByPages;
+
+// // Working With Immutable Arrays
+// // 1) add book object to array
+// const newBook = {
+//   id: 6,
+//   title: 'Harry Potter and the Chamber of Secrets',
+//   author: 'J.K. Rowling'
+// };
+// const booksAfterAdd = [...books, newBook];
+// booksAfterAdd;
+
+// // 2) Delete book object from array
+// const booksAfterDelete = booksAfterAdd.filter(book => book.id !== 3);
+// booksAfterDelete;
+
+// // 3) Update book object in the array
+// const booksAfterUpdate = booksAfterDelete.map(book =>
+//   book.id === 1 ? { ...book, pages: 1210 } : book
+// );
+// booksAfterUpdate;
+
+// Asynchronous JavaScript: Promises
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then(res => res.json())
+//   .then(data => console.log(data));
+
+// console.log('jonas');
+
+// Asynchronous JavaScript: Async/Await
+async function getTodos() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+const todos = getTodos();
+console.log(todos);
+console.log('jonas');
