6fca81589cafb80168313c7ba8b7b117c70d4c57
personal <codewithlinds@gmail.com>
2023-10-03 18:32:48 -0500

    Completed Section 12

--- projects/07-usepopcorn/usepopcorn/src/index.js
+++ projects/07-usepopcorn/usepopcorn/src/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import StarRating from './StarRating';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
 function Test() {
   const [movieRating, setMovieRating] = useState(0);
@@ -18,13 +18,13 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
     />
     <StarRating maxRating={10} defaultRating={5} />
     <StarRating />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
